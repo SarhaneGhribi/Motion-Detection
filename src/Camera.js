5564f98404b38a23b2db46cb4a08362672aaf266
SarhaneGhribi <sarhane.ghribi@gmail.com>
2023-12-12 19:29:32 +0100

    liveness working

--- src/Camera.js
+++ src/Camera.js
@@ -1,30 +1,108 @@
-import { StyleSheet, SafeAreaView, Button } from 'react-native';  // Import Button
-import { useState, useEffect } from 'react';  // Import useEffect
+import { StyleSheet, SafeAreaView, Text, Alert } from 'react-native';
+import { useState, useEffect } from 'react';
 import { Camera, CameraType } from 'expo-camera';
 import * as FaceDetector from 'expo-face-detector';
-
+import { commands } from './constants/constants';
 const CameraComponent = () => {
   const [type, setType] = useState(CameraType.front);
   const [permission, requestPermission] = Camera.useCameraPermissions();
-
+  const [face,setFaces]=useState([]);
+  const [currentCommand, setCurrentCommand] = useState('Turn left');
+  const [completedCommands,setCompletedCommands] = useState([]);
   useEffect(() => {
-    // Request camera permissions when the component mounts
     (async () => {
       const { status } = await requestPermission();
       if (status !== 'granted') {
         alert('Permission to access the camera was denied');
       }
     })();
-  }, []);  // Empty dependency array ensures this effect runs once when the component mounts
-
-  const toggleCameraType = () => {
-    setType(current => (current === CameraType.front ? CameraType.back : CameraType.front));
-  }
-
-  const handleFacesDetected = ({ faces }) => {
-    console.log(faces)
+  }, []);
+const takeSelfie=()=>{
+  console.log("selfie")
+}
+  const handleFacesDetected = ({ faces}) => {
+    setFaces(faces)
+    console.log(face)
   };
+  const renderFaceData = () => {
+    if (!face) {
+      return (
+        <SafeAreaView style={styles.faces}>
+          <Text style={styles.faceDesc}>No face detected</Text>
+        </SafeAreaView>
+      );
+    } else {
+  
+      const checkAllCommandsCompleted = () => {
+        console.log(completedCommands.length)
+        if (completedCommands.length === commands.length) {
+          Alert.alert('Liveness Test', "Liveness Test Successfully completed");
+        };
+      };
+      const getRandomCommand = () => {
+        const filteredCommands = commands.filter(
+          (command) =>
+            !completedCommands.includes(command.command) && command.command !== currentCommand
+        );
+    
+        if (filteredCommands.length === 0) {
+          return null;
+        }
+    
+        const randomIndex = Math.floor(Math.random() * filteredCommands.length);
+        return filteredCommands[randomIndex];
+      };
 
+      const handleCommandCompletion = (command) => {
+        const temp =[command.command];
+        if(completedCommands.length <5);
+        setCompletedCommands(completedCommands.concat(temp));
+        checkAllCommandsCompleted();
+        setCurrentCommand(null);
+      };
+      if (currentCommand === null && face.length > 0) {
+        const command = getRandomCommand();
+        if (command) {
+        const isCommandCompleted = completedCommands.includes(command.command);
+        const commandPrediction = command.prediction(face[0]);
+
+        if (!isCommandCompleted && commandPrediction) {
+          setCurrentCommand(command.command);
+        };
+        };
+      };
+     
+      if (currentCommand && face.length > 0 && completedCommands.length < 5) {
+        const currentCommandData = commands.find(
+          (command) => command.command === currentCommand
+        );
+        if (
+          currentCommandData &&
+          currentCommandData.prediction(face[0]) &&
+          completedCommands.length < 5
+        ) {
+          handleCommandCompletion(currentCommandData);
+  
+          if (completedCommands.length < 5) {
+            const nextCommand = getRandomCommand() || commands.find((cmd) => cmd.command === 'Smile');
+            if (nextCommand) {
+              setCurrentCommand(nextCommand.command);
+  
+              if (nextCommand.command === 'Smile') {
+                takeSelfie();
+              }
+            }
+          }
+        }
+      };
+
+      return (
+        <SafeAreaView style={styles.faces}>
+              <Text style={styles.faceDesc}>{currentCommand}</Text>
+        </SafeAreaView>
+      );
+    };
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Camera
@@ -39,6 +117,7 @@ const CameraComponent = () => {
           tracking: true,
         }}
       />
+      {renderFaceData()}
     </SafeAreaView>
   )
 }
@@ -52,5 +131,18 @@ const styles = StyleSheet.create({
   camera:{
     width: '100%',
     height: '100%',
+  },
+   faces: {
+    position: 'absolute',
+    width: '100%',
+    padding: 10,
+    backgroundColor: "white",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  faceDesc: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: "black",
   }
 })
